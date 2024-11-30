e661ae8321acdf54b72a536bc97bb8a588e0d85a
Axekinn <axel.mendes@esicad.org>
2024-11-30 08:16:06 +0100

    enlèvement du bot

--- config/secrets.py
+++
@@ -1,2 +0,0 @@
-TOKEN = 'MTI0MDMwNzMxMjIxNjg5OTY5Ng.GNTxVT.dCzxMNhB6vUX4in72hJLXYoZcJm0BWDoGmlTsI' #Le token de ton bot discord
-MONGO_URI = '' #L'url de ta base de donnée mongoDB, ne mettez rien si vous n'en avez pas
--- ext/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import audit_log
-from . import database
-from . import embeds
-from . import functions
